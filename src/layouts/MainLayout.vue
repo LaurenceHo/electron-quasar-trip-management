39320883d66615c9441de308a4bcbe9f52633eb5
Laurence Ho <bluegray.he@aol.com>
2021-04-18 13:31:18 +1200

    Init commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,75 @@
+<template>
+  <q-layout view="hHh lpR fFf" style="background: #fff">
+    <q-drawer
+      show-if-above
+      :mini="miniState"
+      @click.capture="drawerClick"
+      :width="200"
+      :breakpoint="500"
+      bordered
+      class="bg-grey-3"
+    >
+      <q-scroll-area class="fit">
+        <q-list padding>
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="mdi-inbox" />
+            </q-item-section>
+            <q-item-section> Inbox </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="mdi-star-outline" />
+            </q-item-section>
+            <q-item-section> Star </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="mdi-send" />
+            </q-item-section>
+            <q-item-section> Send </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="mdi-email-open" />
+            </q-item-section>
+            <q-item-section> Drafts </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+
+      <div class="q-mini-drawer-hide absolute" style="top: 15px; right: -17px">
+        <q-btn dense round unelevated color="secondary" icon="mdi-chevron-left" @click="miniState = true" />
+      </div>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  setup() {
+    const miniState = ref(false);
+
+    return {
+      miniState,
+      drawerClick(e: Event) {
+        if (miniState.value) {
+          miniState.value = false;
+          e.stopPropagation();
+        }
+      },
+    };
+  },
+});
+</script>
